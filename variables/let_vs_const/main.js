9541a990bff0519c9f8830f10407793a81f7e84b
nicl09 <niclas.behrendt@nicl.dev>
2022-06-03 22:33:09 +0200

    finished let_vs_const

---
+++ variables/let_vs_const/main.js
@@ -0,0 +1,15 @@
+"use strict";
+
+let my_variable = 'JavaScript';
+console.log(my_variable);
+my_variable = 42;
+console.log(my_variable);
+
+const seconds_in_hour = 3600;
+console.log(seconds_in_hour);
+// Must not declare const twice
+// seconds_in_hour = 3500;
+
+// const always have to be initialized!
+// const pi;
+const pi = 3.14;
