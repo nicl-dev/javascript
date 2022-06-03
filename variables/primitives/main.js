1aae4f108dd39310381880948ec90752e84003e7
nicl09 <niclas.behrendt@nicl.dev>
2022-06-03 21:07:54 +0200

    finished primitives

---
+++ variables/primitives/main.js
@@ -0,0 +1,16 @@
+"use strict";
+
+// Strings
+let name = "Gandalf";
+let name2 = 'Saruman';
+let code = 'let test = 42';
+
+// Numbers
+let age = 30;
+let distance_in_km = 0.5;
+
+// Booleans
+let i_like_javascript = true;
+let i_like_spiders = false;
+
+console.log(name, name2, code, age, i_like_javascript, i_like_spiders);
