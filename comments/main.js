aae8594763adc523e5f20dff0c06423f25d2a9dd
nicl09 <niclas.behrendt@nicl.dev>
2022-06-03 22:55:26 +0200

    finished comments

---
+++ comments/main.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// Posts 'Hello" into console. <- Single-line comment describing code
+// console.log('Hello'); <- Also used to comment out code
+
+/* 
+*
+* Multi-line comment
+*
+* Use stars to make it easier visually
+*
+*
+*/
