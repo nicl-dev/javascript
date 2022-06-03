a4b4301d1c484484a8b1bef9b0b45d96ca505adf
nicl09 <niclas.behrendt@nicl.dev>
2022-06-03 22:49:37 +0200

    finished prompts

---
+++ variables/prompt/main.js
@@ -0,0 +1,10 @@
+"use strict";
+
+let before = 'Before the prompt...';
+console.log(before);
+
+let name = prompt("What's your name?");
+console.log(name);
+
+let after = 'After the prompt...';
+console.log(after);
